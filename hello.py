fe7f82112f97383c7aa2d10d7b769fb534c9aa92
limys430 <limys430@gmail.com>
2020-04-22 15:14:52 +0900

    feat: when multiple of 3 print fizz, 5 print buzz both fizbuz

--- hello.py
+++ hello.py
@@ -1,7 +1,13 @@
 
 for i in range(1, 100):
-    if i%2==0:
-        print("hello World")
-    elif i%3==0:
+    if i%3==0 and i%5 !=0:
         print("fizz")
 
+    elif i%3!=0 and i%5==0:
+        print("buzz")
+
+    elif i%3==0 and i%5==0:
+        print("fizzbuzz")
+    
+    else:
+        print(i)
